9b20b6ae17c6c2aff8a7d6698a0b5d1a652f4c64
13816025562@139.com <13816025562@139.com>
2020-11-27 23:20:46 +0800

    ft: ready data

--- examples/views/table/tree/Highlight.vue
+++ examples/views/table/tree/Highlight.vue
@@ -5,7 +5,12 @@
       <span class="red">(注：树结构不支持大量数据，如果数据量超过 500 条，请谨慎使用！)</span>
     </p>
 
+    <p>
+      <vxe-button @click="exportDataEvent">导出</vxe-button>
+    </p>
+
     <vxe-table
+      ref="xTable"
       highlight-current-row
       :data="tableData"
       :tree-config="{children: 'children', navColumn: [2,2]}"
@@ -20,6 +25,9 @@
       <vxe-table-column field="cl" title="材料" :edit-render="{name: 'input'}"></vxe-table-column>
       <vxe-table-column field="gy" title="工艺" :edit-render="{name: 'input'}"></vxe-table-column>
       <vxe-table-column field="djzl" title="单件重量" :edit-render="{name: 'input'}"></vxe-table-column>
+      <vxe-table-column field="role" title="角色" :edit-render="{name: '$select', options: roleList, props: {clearable: true, multiple: true}, events: {change: roleChangeEvent}}"></vxe-table-column>
+      <vxe-table-column field="ddtime" title="日期" :edit-render="{name: '$input', props: {type: 'date'}, events: {change: roleChangeEvent} }"></vxe-table-column>
+
 <!--      <vxe-table-column field="name" title="Name" tree-node></vxe-table-column>-->
 <!--      <vxe-table-column field="size" title="Size"></vxe-table-column>-->
 <!--      <vxe-table-column field="type" title="Type"></vxe-table-column>-->
@@ -53,7 +61,34 @@ export default {
     return {
       tableData: [],
       selectRow: null,
-      demoCodes: []
+      demoCodes: [],
+      roleList: [
+        {
+          label: '前端',
+          value: '1',
+          disabled: false
+        },
+        {
+          label: '后端',
+          value: '2',
+          disabled: false
+        },
+        {
+          label: '项目经理',
+          value: '3',
+          disabled: false
+        },
+        {
+          label: '设计师',
+          value: '4',
+          disabled: false
+        },
+        {
+          label: '运维',
+          value: '5',
+          disabled: false
+        }
+      ]
     }
   },
   created () {
@@ -65,6 +100,8 @@ export default {
         cl: ' 金属',
         gy: '铰链冲压计算公式',
         djzl: '1.5',
+        role: '运维',
+        ddtime: '2020-01-01',
         children: [
           {
             // id: 1100,
@@ -72,7 +109,9 @@ export default {
             ljmc: '新风口密封条',
             cl: ' 金属',
             gy: '铰链冲压计算公式',
-            djzl: '1.5'
+            djzl: '1.5',
+            role: 1,
+            ddtime: '2020-03-01'
           }, {
             // id: 1200,
             // parentId: 1000,
@@ -80,18 +119,24 @@ export default {
             cl: ' 金属',
             gy: '铰链冲压计算公式',
             djzl: '1.5',
+            role: 2,
+            ddtime: '2020-04-01',
             children: [
               {
                 ljmc: '螺钉-内外循环内门执行器aa',
                 cl: ' 金属',
                 gy: '铰链冲压计算公式',
-                djzl: '1.5'
+                djzl: '1.5',
+                role: 3,
+                ddtime: '2020-05-01'
               },
               {
                 ljmc: '螺钉-内外循环内门执行器aa',
                 cl: ' 金属',
                 gy: '铰链冲压计算公式',
-                djzl: '1.5'
+                djzl: '1.5',
+                role: 3,
+                ddtime: '2020-06-01'
               }
             ]
           },
@@ -101,7 +146,9 @@ export default {
             ljmc: '新风口密封条world',
             cl: ' 金属',
             gy: '铰链冲压计算公式',
-            djzl: '1.5'
+            djzl: '1.5',
+            role: 2,
+            ddtime: '2020-07-01'
           }
         ]
       },
@@ -110,7 +157,9 @@ export default {
         ljmc: '螺钉-新风口到鼓机单元',
         cl: ' 金属',
         gy: '铰链冲压计算公式',
-        djzl: '1.5'
+        djzl: '1.5',
+        role: 1,
+        ddtime: '2020-09-01'
       },
       {
         // id: 3000,
@@ -118,23 +167,31 @@ export default {
         cl: ' 金属',
         gy: '铰链冲压计算公式',
         djzl: '1.5',
+        role: 2,
+        ddtime: '2020-02-01',
         children: [
           {
             ljmc: '新风口密封条',
             cl: ' 金属',
             gy: '铰链冲压计算公式',
             djzl: '1.5',
+            role: 4,
+            ddtime: '2020-02-01',
             children: [{
               ljmc: 'aaa',
               cl: ' bb',
               gy: '铰链冲压计算公式',
-              djzl: '1.5'
+              djzl: '1.5',
+              role: 4,
+              ddtime: '2020-01-01'
             },
             {
               ljmc: 'bbbb',
               cl: ' aa',
               gy: '铰链冲压计算公式',
-              djzl: '1.5'
+              djzl: '1.5',
+              role: 5,
+              ddtime: '2020-08-01'
             }
             ]
           }, {
@@ -142,34 +199,46 @@ export default {
             cl: ' 金属',
             gy: '铰链冲压计算公式',
             djzl: '1.5',
+            role: 2,
+            ddtime: '2020-09-01',
             children: [
               {
                 ljmc: '螺钉-内外循环内门执行器bb',
                 cl: ' 金属',
                 gy: '铰链冲压计算公式',
-                djzl: '1.5'
-              }, {
-                ljmc: '螺钉-内外循环内门执行器bb',
-                cl: ' 金属',
-                gy: '铰链冲压计算公式',
-                djzl: '1.5'
+                djzl: '1.5',
+                role: 4,
+                ddtime: '2020-01-01'
               }, {
                 ljmc: '螺钉-内外循环内门执行器bb',
                 cl: ' 金属',
                 gy: '铰链冲压计算公式',
                 djzl: '1.5',
+                role: 5,
+                ddtime: '2020-02-01'
+              }, {
+                ljmc: '螺钉-内外循环内门执行器bb',
+                cl: ' 金属',
+                gy: '铰链冲压计算公式',
+                djzl: '1.5',
+                role: 1,
+                ddtime: '2020-09-01',
                 children: [
                   {
                     ljmc: '螺钉-内外循环内门执行器bbb',
                     cl: ' 金属',
                     gy: '铰链冲压计算公式',
                     djzl: '1.5',
+                    role: 2,
+                    ddtime: '2020-01-01',
                     children: [
                       {
                         ljmc: '螺钉-内外循环内门执行器bbbb',
                         cl: ' 金属',
                         gy: '铰链冲压计算公式',
-                        djzl: '1.5'
+                        djzl: '1.5',
+                        role: 3,
+                        ddtime: '2020-04-01'
                       }
                     ]
                   }
@@ -182,28 +251,39 @@ export default {
             cl: ' 金属',
             gy: '铰链冲压计算公式',
             djzl: '1.5',
+            role: 2,
+            ddtime: '2020-01-01',
             children: [
               {
                 ljmc: '螺钉-123内外循2环内门执行器zz',
                 cl: ' 金属',
                 gy: '铰链冲压计算公式',
-                djzl: '1.5'
-              }, {
+                djzl: '1.5',
+                role: 1,
+                ddtime: '2020-10-01'
+              },
+              {
                 ljmc: '螺钉-123内外循2环内门执行器zz',
                 cl: ' 金属',
                 gy: '铰链冲压计算公式',
                 djzl: '1.5',
+                role: 2,
+                ddtime: '2020-02-01',
                 children: [
                   {
                     ljmc: '螺钉-123内外循2环内门执行器zzz',
                     cl: ' 金属',
                     gy: '铰链冲压计算公式',
                     djzl: '1.5',
+                    role: 3,
+                    ddtime: '2020-03-04',
                     children: [{
                       ljmc: '螺钉-123内外循2环内门执行器zzzz',
                       cl: ' 金属',
                       gy: '铰链冲压计算公式',
-                      djzl: '1.5'
+                      djzl: '1.5',
+                      role: 2,
+                      ddtime: '2020-02-11'
                     }]
                   }
                 ]
@@ -217,14 +297,18 @@ export default {
         ljmc: '新风单元及蒸发器单元',
         cl: ' 塑料',
         gy: '铰链冲压计算公式',
-        djzl: '1.5'
+        djzl: '1.5',
+        role: 2,
+        ddtime: '2020-08-13'
       },
       {
         // id: 5000,
         ljmc: '新风单元及蒸发器单元',
         cl: ' 塑料',
         gy: '铰链冲压计算公式',
-        djzl: '1.5'
+        djzl: '1.5',
+        role: 1,
+        ddtime: '2020-07-26'
       }
     ]
   },
@@ -232,6 +316,29 @@ export default {
     Array.from(this.$el.querySelectorAll('pre code')).forEach((block) => {
       hljs.highlightBlock(block)
     })
+  },
+  methods: {
+    updateRoleList () {
+      // 获取表格中的全量数据
+      const { fullData } = this.$refs.xTable.getTableData()
+      this.roleList.forEach(item => {
+        if (item.value) {
+          // 如果当前选项已经被选过，则禁用
+          item.disabled = fullData.some(row => row.role === item.value)
+        }
+      })
+    },
+    roleChangeEvent (e) {
+      // this.updateRoleList()
+      console.log(e)
+    },
+    exportDataEvent () {
+      this.$refs.xTable.exportData({
+        filename: '导出test',
+        sheetName: 'Sheet1',
+        type: 'xlsx'
+      })
+    }
   }
 }
 </script>
